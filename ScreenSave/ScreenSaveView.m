eb4e357d37a18294bd2a3d18cc86cd13bef62d16
GraceBK <iris.b1991@gmail.com>
2016-11-05 14:16:12 +0100

    Update

--- ScreenSave/ScreenSaveView.m
+++ ScreenSave/ScreenSaveView.m
@@ -36,6 +36,49 @@
 
 - (void)animateOneFrame
 {
+    NSBezierPath *path;
+    NSRect rect;
+    NSSize size;
+    NSColor *color;
+    float red, green, blue, alpha;
+    int shapeType;
+    
+    size = [self bounds].size;
+    // Calcul random width et height
+    rect.size = NSMakeSize(SSRandomFloatBetween(size.width / 100.0, size.height / 100.0), SSRandomFloatBetween(size.width / 100.0, size.height / 100.0));
+    // Calcul random origin point
+    rect.origin = SSRandomPointForSizeWithinRect(rect.size, [self bounds]);
+    
+    // Draw
+    shapeType = SSRandomIntBetween(0, 2);
+    switch (shapeType) {
+        case 0:
+            path = [NSBezierPath bezierPathWithRect:rect];
+            break;
+        case 1:
+            path = [NSBezierPath bezierPathWithOvalInRect:rect];
+            break;
+            
+        default:
+            break;
+    }
+    
+    // Color
+    red = SSRandomFloatBetween(0.0, 255.0) / 255.0;
+    green = SSRandomFloatBetween(0.0, 255.0) / 255.0;
+    blue = SSRandomFloatBetween(0.0, 255.0) / 255.0;
+    alpha = SSRandomFloatBetween(0.0, 255.0) / 255.0;
+    
+    color = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha];
+    
+    [color set];
+    
+    // And finally draw it
+    if (SSRandomIntBetween(0, 1) == 0) {
+        [path fill];
+    } else {
+        [path stroke];
+    }
     return;
 }
 
