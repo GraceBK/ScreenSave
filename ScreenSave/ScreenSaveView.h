0ed13d4f686397cc52aad3c6784fc939651fc2d8
GraceBK <iris.b1991@gmail.com>
2016-11-05 14:00:30 +0100

    Initial Commit

---
+++ ScreenSave/ScreenSaveView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenSaveView.h
+//  ScreenSave
+//
+//  Created by Grâce BOUKOU on 05/11/2016.
+//  Copyright © 2016 Grace BOUKOU. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenSaveView : ScreenSaverView
+
+@end
